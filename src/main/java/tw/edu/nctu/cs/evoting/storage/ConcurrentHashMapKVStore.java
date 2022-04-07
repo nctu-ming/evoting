8556f43e977063b092eba4b54bfc0fb11fd51b0c
Ming <flyindance@love.com>
2022-04-07 17:51:50 +0800

    Modify key type: byte[] to String

--- src/main/java/tw/edu/nctu/cs/evoting/storage/ConcurrentHashMapKVStore.java
+++ src/main/java/tw/edu/nctu/cs/evoting/storage/ConcurrentHashMapKVStore.java
@@ -2,25 +2,25 @@ package tw.edu.nctu.cs.evoting.storage;
 
 import java.util.concurrent.ConcurrentHashMap;
 
-public class ConcurrentHashMapKVStore implements KVStore<byte[], byte[]> {
-    final private ConcurrentHashMap<byte[], byte[]> syncMap;
+public class ConcurrentHashMapKVStore implements KVStore<String, byte[]> {
+    final protected ConcurrentHashMap<String, byte[]> syncMap;
 
     public ConcurrentHashMapKVStore() {
         this.syncMap = new ConcurrentHashMap<>();
     }
 
     @Override
-    public void put(byte[] key, byte[] value) {
+    public void put(String key, byte[] value) {
         this.syncMap.put(key, value);
     }
 
     @Override
-    public byte[] get(byte[] key) {
+    public byte[] get(String key) {
         return this.syncMap.get(key);
     }
 
     @Override
-    public void delete(byte[] key) {
+    public void delete(String key) {
         this.syncMap.remove(key);
     }
 }
