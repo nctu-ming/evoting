a9316a888fac7a2f736e8831652a01430ac1a6e6
Ming <flyindance@love.com>
2022-04-09 11:22:09 +0800

    ConcurrentHashMapKVStore to LevelDBStore

--- src/main/java/tw/edu/nctu/cs/evoting/Globals.java
+++ src/main/java/tw/edu/nctu/cs/evoting/Globals.java
@@ -1,10 +1,10 @@
 package tw.edu.nctu.cs.evoting;
 
-import tw.edu.nctu.cs.evoting.storage.ConcurrentHashMapKVStore;
 import tw.edu.nctu.cs.evoting.storage.KVStore;
+import tw.edu.nctu.cs.evoting.storage.LevelDBKVStore;
 import tw.edu.nctu.cs.evoting.util.JwtManager;
 
 public class Globals {
-    protected static final KVStore<String, byte[]> store = new ConcurrentHashMapKVStore();
+    protected static final KVStore<String, byte[]> store = new LevelDBKVStore();
     protected static final JwtManager jwtManager = JwtManager.EVotingJwtManager();
 }
